6f71bab51e128799f81ae405169b0604937b2703
kojeje <kojeje.dev@gmail.com>
2018-11-22 14:38:55 +0100

    creation 3 tables repository doctrine A commenter et comprendre insertion de commentaires dynamiques Auteur Repository

---
+++ src/AppBundle/Repository/AuteurRepository.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: jeromesuhard
+     * Date: 22/11/2018
+     * Time: 14:34
+     */
+
+
+    namespace AppBundle\Repository;
+
+
+    use Doctrine\ORM\EntityRepository;
+
+    class AuteurRepository extends EntityRepository
+    {
+
+}
---
+++ src/AppBundle/Repository/LivreRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeromesuhard
+ * Date: 22/11/2018
+ * Time: 09:59
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class LivreRepository extends EntityRepository
+{
+
+}
